1f855f00fb42ba1d7858d583edf7949b068b0989
Srivatsan <talkads123@gmail.com>
2025-02-11 21:19:03 +0000

    add constructors getters setters and member functions implementation

---
+++ .ccls-cache/@home@runner@workspace/Book.hpp
@@ -0,0 +1,34 @@
+#ifndef BOOK_HPP
+#define BOOK_HPP
+
+#include <string>
+
+class Book
+{
+public:
+  /// Constructors:
+  Book();
+  Book(std::string title, std::string author, std::string isbn);
+
+  /// Getters:
+  std::string getTitle() const;
+  std::string getAuthor() const;
+  std::string getISBN() const;
+
+  /// Setters:
+  void setTitle(std::string title);
+  void setAuthor(std::string author);
+  void setISBN(std::string isbn);
+
+  /// Member functions:
+
+  /// Converts book information to string
+  std::string toString() const;
+
+private:
+	std::string title;
+  std::string author;
+  std::string isbn;
+};
+
+#endif // BOOK_HPP
