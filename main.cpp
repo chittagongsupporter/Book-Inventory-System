b229611994f873172bbd1090ad8744ed5dd59aeb
Srivatsan <talkads123@gmail.com>
2025-02-11 21:22:44 +0000

    Test getters

--- main.cpp
+++ main.cpp
@@ -3,5 +3,9 @@
 
 int main() {
   Book book1( "The Great Gatsby", "F. Scott Fitzgerald", "978-3-16-148410-0");
-  
+
+  // Test getters
+  std::cout << "Title: " << book1.getTitle() << std::endl;
+  std::cout << "Author: " << book1.getAuthor() << std::endl;
+  std::cout << "ISBN: " << book1.getISBN() << std::endl;
 }
