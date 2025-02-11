3bd7ab8f1f510a1ebd1c1ac10694c7fb9667e622
Srivatsan <talkads123@gmail.com>
2025-02-11 21:10:40 +0000

    add skeleton structure and private data members for Book.hpp

---
+++ Book.hpp
@@ -0,0 +1,16 @@
+#ifndef BOOK_HPP
+#define BOOK_HPP
+
+#include <string>
+
+class Book
+{
+public:
+
+private:
+	std::string title;
+  std::string author;
+  std::string isbn;
+};
+
+#endif // BOOK_HPP
