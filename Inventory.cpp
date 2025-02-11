02264f4f198cca86f4bea93e45507adc1567e79a
Srivatsan <talkads123@gmail.com>
2025-02-11 21:41:58 +0000

    add getters and setters implementation

--- Inventory.cpp
+++ Inventory.cpp
@@ -3,3 +3,8 @@
 // Constructors:
 Inventory::Inventory() {}
 Inventory::Inventory(std::vector<Book> bookList) { setBookList(bookList); }
+
+// Getters:
+std::vector<Book> Inventory::getBookList() const { return books; }
+// Setters:
+void Inventory::setBookList(std::vector<Book> bookList) { books = bookList; }
