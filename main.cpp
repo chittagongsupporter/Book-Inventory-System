0c861c7f52197e34a7691d66bf090f083923362a
Srivatsan <talkads123@gmail.com>
2025-02-12 00:21:33 +0000

    Replace main function to test Inventory functionality
    - Updated the main function to call `testInventoryObject` for testing the Inventory class.
    - Commented out the call to `testBookObject` to focus on testing the Inventory class.
    - Introduced new `testInventoryObject` function to test adding books to an Inventory.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
-#include "Book.hpp"
+#include "Inventory.hpp"
 #include <iostream>
+#include <vector>
 
 void testBookObject() {
   Book book1("The Great Gatsby", "F. Scott Fitzgerald", "978-3-16-148410-0");
@@ -17,6 +18,23 @@ void testBookObject() {
   std::cout << book1.toString() << std::endl;
 }
 
+void testInventoryObject() {
+  // Create Book objects
+  Book book1("The Great Gatsby", "F. Scott Fitzgerald", "978-3-16-148410-0");
+  Book book2("To Kill a Mockingbird", "Harper Lee", "978-0-393-72564-8");
+
+  // Create Inventory object
+  Inventory inventory;
+  // Add Book objects to Inventory
+  inventory.addBook(book1);
+  inventory.addBook(book2);
+  // Test getters
+  std::cout << "Number of books in inventory: " << inventory.getBookCount()
+            << std::endl;
+}
+
 int main() {
-  testBookObject();
+  // testBookObject();
+  testInventoryObject();
+  return 0;
 }
