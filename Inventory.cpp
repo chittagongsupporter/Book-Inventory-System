7ffc9e1f75814a4e68e65cfb30787b5758d8579e
Srivatsan <talkads123@gmail.com>
2025-02-12 02:07:17 +0000

    modify whitespace

--- Inventory.cpp
+++ Inventory.cpp
@@ -4,6 +4,7 @@
 
 // Constructors:
 Inventory::Inventory() = default;
+
 Inventory::Inventory(std::vector<Book> bookList) : books(std::move(bookList)) {}
 
 // Getter:
@@ -14,6 +15,7 @@ const std::vector<Book> &Inventory::getBookList() const { return books; }
 void Inventory::setBookList(const std::vector<Book> &bookList) {
   books = bookList;
 }
+
 // move overload
 void Inventory::setBookList(std::vector<Book> &&bookList) {
   books = std::move(bookList);
@@ -23,6 +25,7 @@ void Inventory::setBookList(std::vector<Book> &&bookList) {
 
 // addBook copy overload
 void Inventory::addBook(const Book &book) { books.push_back(book); }
+
 // addBook move overload
 void Inventory::addBook(Book &&book) { books.push_back(std::move(book)); }
 
@@ -31,36 +34,44 @@ void Inventory::removeBook(const std::string &isbn) {
   auto hasMatchingISBN = [&isbn](const Book &currentBookInVector) {
     return currentBookInVector.getISBN() == isbn;
   };
+
   auto newEndIterator =
       std::remove_if(books.begin(), books.end(), hasMatchingISBN);
+
   books.erase(newEndIterator, books.end());
 }
 
 std::vector<Book> Inventory::searchBookByTitle(const std::string &title) const {
   std::vector<Book> result;
+
   for (const auto &book : books) {
     if (book.getTitle() == title)
       result.push_back(book);
   }
+
   return result;
 }
 
 std::vector<Book>
 Inventory::searchBookByAuthor(const std::string &author) const {
   std::vector<Book> result;
+
   for (const auto &book : books) {
     if (book.getAuthor() == author)
       result.push_back(book);
   }
+
   return result;
 }
 
 std::vector<Book> Inventory::searchBookByISBN(const std::string &isbn) const {
   std::vector<Book> result;
+
   for (const auto &book : books) {
     if (book.getISBN() == isbn)
       result.push_back(book);
   }
+
   return result;
 }
 
