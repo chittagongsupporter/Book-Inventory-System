8c30d7f9df469cbb0041d2152a3b0bbc75e593c0
Srivatsan <talkads123@gmail.com>
2025-02-12 02:05:45 +0000

    modify whitespace

--- .ccls-cache/@home@runner@workspace/Book.cpp
+++ .ccls-cache/@home@runner@workspace/Book.cpp
@@ -2,18 +2,23 @@
 
 // Constructors:
 Book::Book() : title(""), author(""), isbn("") {}
+
 Book::Book(std::string title, std::string author, std::string isbn)
     : title(std::move(title)), author(std::move(author)),
       isbn(std::move(isbn)) {}
 
 // Getters:
 const std::string &Book::getTitle() const { return title; }
+
 const std::string &Book::getAuthor() const { return author; }
+
 const std::string &Book::getISBN() const { return isbn; }
 
 // Setters:
 void Book::setTitle(const std::string &title) { this->title = title; }
+
 void Book::setAuthor(const std::string &author) { this->author = author; }
+
 void Book::setISBN(const std::string &isbn) { this->isbn = isbn; }
 
 // Member functions:
